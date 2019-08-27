0669e58faa16d612e3649fe095c1fff0f0dcaeda
Basavaraj <saralahasare86@gmail.com>
2019-08-27 17:50:15 +0530

    firefox

--- src/test/java/Demo1/DemoProg.java
+++ src/test/java/Demo1/DemoProg.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class DemoProg 
@@ -22,10 +23,9 @@ public class DemoProg
 	public static void A() throws InterruptedException
 	{
 
-//		ChromeOptions opt=new ChromeOptions();
-//		opt.setBinary(path)
 	
-		WebDriver driver = new ChromeDriver();
+	
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
